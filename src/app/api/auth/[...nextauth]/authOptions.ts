6e86ecc4f89a411e6323a6bcd7e366e43b65e8ca
SI_hub_4002 <iwasora2004@icloud.com>
2024-11-20 23:28:48 +0900

    delete email authentication

--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,7 +1,6 @@
 import type { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import GitHubProvider from "next-auth/providers/github";
-import EmailProvider from "next-auth/providers/email";
 import prisma from "../../../../../lib/prisma";
 
 export const authOptions: NextAuthOptions = {
@@ -10,10 +9,6 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    EmailProvider({
-			server: process.env.EMAIL_SERVER,
-			from: process.env.EMAIL_FROM,
-		}),
   ],
   debug: true,
   adapter: PrismaAdapter(prisma),
--- src/components/features/SignIn.tsx
+++ src/components/features/SignIn.tsx
@@ -3,14 +3,9 @@
 import Button from "@/components/elements/Button";
 import { getProviders, signIn } from "next-auth/react"
 import { useEffect, useState } from "react";
-import Input from "../elements/Input";
-import Link from "next/link";
 
 export default function SignIn() {
     const [providers, setProviders] = useState<object | null>(null);
-    const [email, setEmail] = useState("");
-    const [isEmailSent, setIsEmailSent] = useState(false);
-    const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchProviders = async () => {
@@ -19,25 +14,6 @@ export default function SignIn() {
         };
         fetchProviders();
     }, []);
-
-    const handleEmailSignIn = async () => {
-        if (email) {
-            setError(null);
-            try {
-                const result = await signIn("email", { email, callbackUrl: "/" });
-
-                if (result?.error) {
-                    setError("Failed to send email. Please try again.");
-                } else {
-                    setIsEmailSent(true); 
-                }
-            } catch {
-                setError("An error occurred while signing in.");
-            }
-        } else {
-            setError("Please enter a valid email address.");
-        }
-    };
 
     return (
         <div className="absolute w-[380px] sm:w-[420px] h-[calc(100vh-80px)] left-1/2 -translate-x-1/2 p-6 pt-10 sm:pt-16 text-gray-700">
@@ -64,32 +40,6 @@ export default function SignIn() {
                         return null;
                     })
                 }
-                <div className="flex flex-col gap-7 border-t pt-5 pb-5">
-                    <div className="flex flex-col gap-0">
-                        <span className="font-bold text-sm pb-2">Email address</span>
-                        <Input
-                            className="border text-base border-gray-300 focus:outline-none p-1 pl-2 pr-2 w-72"
-                            type="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="Enter your email"
-                        />
-                    </div>
-                    <Button className="p-1 text-sm text-white bg-gray-700 hover:bg-gray-600" onClick={handleEmailSignIn}>
-                        {isEmailSent ? "Check your inbox" : "Continue"}
-                    </Button>
-                    {isEmailSent && (
-                        <div className="text-sm text-gray-500 mt-3">
-                            Please check your inbox for the sign-in link.
-                        </div>
-                    )}
-                    {error && <div className="text-red-500 text-sm mt-2">{error}</div>}
-                </div>
-                <div className="border-t">
-                    <span className="text-gray-400 text-sm">
-                        Already have an account? {<Link href="/signup" className="text-gray-600 font-bold hover:text-gray-400">Sign up</Link>}
-                    </span>
-                </div>
             </div>
         </div>
     )
