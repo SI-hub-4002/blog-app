d27784c3ac579d068626a8ce183e23311ee73304
SI_hub_4002 <iwasora2004@icloud.com>
2024-11-21 09:24:28 +0900

    modified signin css

--- src/components/features/SignIn.tsx
+++ src/components/features/SignIn.tsx
@@ -17,11 +17,10 @@ export default function SignIn() {
 
     return (
         <div className="absolute w-[380px] sm:w-[420px] h-[calc(100vh-80px)] left-1/2 -translate-x-1/2 p-6 pt-10 sm:pt-16 text-gray-700">
-            <div className="bg-white shadow-lg flex flex-col p-4 items-center text-2xl gap-2">
-                <div className="flex flex-col justify-center items-center">
+            <div className="bg-white shadow-lg flex flex-col p-4 items-center text-2xl gap-3">
+                <div className="flex flex-col justify-center items-center pb-4 border-b w-full">
                     <span className="font-bold text-3xl pb-2">N-Blog</span>
-                    <span className="font-bold text-xl">Sign in to N-Blog</span>
-                    <span className="text-sm text-gray-400">Welcome back! Please sign in to continue</span>
+                    <span className="font-bold text-xl">Welcom to N-Blog</span>
                 </div>
                 {providers &&
                     Object.values(providers).map((provider) => {
