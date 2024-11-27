48aed9c8eff42d68dc141229d43b5f58b2217b0e
SI_hub_4002 <iwasora2004@icloud.com>
2024-11-27 12:54:01 +0900

    modified README

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
     images: {
         domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
     },
-    experimental: {
-        vercelToolkit: false, 
-    },
 };
 
 export default nextConfig;
