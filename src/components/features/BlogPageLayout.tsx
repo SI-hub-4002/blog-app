334b76d7766d6e6646ceea264b832cbe96260196
SI_hub_4002 <iwasora2004@icloud.com>
2024-11-27 15:13:11 +0900

    modified Blogpage css

--- src/components/features/BlogPageLayout.tsx
+++ src/components/features/BlogPageLayout.tsx
@@ -98,7 +98,7 @@ export default function BlogPageLayout({ postsProps, usersProps }: BlogPageLayou
                         </div>
                         {uniquePostData.createdAt.toLocaleDateString()}
                     </div>
-                    <div className="pt-4 pl-2 pr-2 text-base sm:text-xl overflow-hidden break-words">
+                    <div className="pt-4 pl-2 pr-2 text-base sm:text-xl break-words">
                         {uniquePostData.content}
                     </div>
                 </div>
